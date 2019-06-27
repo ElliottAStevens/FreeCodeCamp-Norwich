e2b61a15fd3eea440e1a203d95b9b7e31a4e0e15
Elliott Stevens <elliott.a.stevens1@gmail.com>
2019-06-27 12:58:53 +0100

    Home page
    Created homepage.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const links = document.getElementsByClassName("navbar-links")[0];
+const content = document.getElementsByClassName("content")[0];
 
 toggleButton.addEventListener("click", function() {
   links.classList.toggle("active");
+  content.classList.toggle("down");
 });
+
+
+
+  
