b0bd3090f46438a8ec7d5aaacf8704afa8dc8ed5
Elliott Stevens <elliott.a.stevens1@gmail.com>
2019-06-29 17:28:07 +0100

    Added Socials Page
    added new page for social media and chat.
    fixed bug in JavaScript.
    curriculum page is now more responsive.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const links = document.getElementsByClassName("navbar-links")[0];
-const content = document.getElementsByClassName("content")[0];
+const main = document.getElementsByTagName("main")[0];
 
 toggleButton.addEventListener("click", function() {
   links.classList.toggle("active");
-  content.classList.toggle("down");
+  main.classList.toggle("down");
 });
 
 
